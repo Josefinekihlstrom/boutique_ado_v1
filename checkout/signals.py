27bf20a4b13af91f504184591a8d73ecf2dc1cdf
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2021-02-05 15:37:25 +0000

    Added checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,17 +3,16 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
-@reciever(post_save, sender=OrderLineItem)
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
-   """
-   Update order total on lineitem update/create
-   """
-   instance.order.update_total()
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
 
-@reciever(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
-   """
-   Update order total on lineitem delete
-   """
-   instance.order.update_total()
-   
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
